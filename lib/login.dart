c1a757e821a05ced6a09ef10a3a1f939d67075d8
viettung <viettungvuong@gmail.com>
2022-06-03 22:46:11 +0700

    Them ham dang nhap

--- lib/login.dart
+++ lib/login.dart
@@ -4,9 +4,13 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import 'main.dart';
+
 User fromMapUser(Map<dynamic, dynamic> document) {
   late User usr;
-  usr = new User(document['username'], document['password']);
+  if (document.isNotEmpty) {
+    usr = User(document['username'], document['password']);
+  }
   return usr;
 }
 
@@ -18,13 +22,13 @@ class User {
 }
 
 class UserList {
-  late List<User> users;
+  static late Map<String, User> users;
   Future<void> getUsers() async {
     var collection = FirebaseFirestore.instance.collection('users');
     collection.get().then((value) => {
           value.docs.forEach((element) {
             setState() {
-              users.add(fromMapUser(element.data()));
+              users[element.data()['username']] = fromMapUser(element.data());
             }
           })
         });
@@ -39,7 +43,25 @@ class loginWindow extends StatefulWidget {
 }
 
 class _loginState extends State<loginWindow> {
-  void login() {}
+  final TextEditingController _controller1 = TextEditingController();
+  final TextEditingController _controller2 = TextEditingController();
+
+  void register(String username) {}
+
+  void login() {
+    if (UserList.users[_controller1.text] == null) {
+      //chua co user trong he thong
+      register(_controller1.text);
+    } else {
+      if (_controller2.text == UserList.users[_controller1.text]?.password) {
+        //them dau cham hoi vi co the se null
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => mainWidget()),
+        );
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,6 +84,7 @@ class _loginState extends State<loginWindow> {
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
                 ),
+                controller: _controller1,
               ),
             ),
             Container(
@@ -74,6 +97,7 @@ class _loginState extends State<loginWindow> {
               padding: EdgeInsets.only(left: 30, bottom: 40, right: 0),
               width: 300,
               child: TextField(
+                controller: _controller2,
                 obscureText: true,
                 enableSuggestions: false,
                 autocorrect: false,
