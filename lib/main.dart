d876fd4e46ab4296dfe7ef31111b04476d353192
viettung <viettungvuong@gmail.com>
2022-06-08 20:23:07 +0700

    Them auto login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:hnh/chat.dart';
 import 'package:hnh/chat_page.dart';
@@ -15,9 +16,10 @@ void main(List<String> args) async {
     name: 'HNH',
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  currentUsr = FirebaseAuth.instance.currentUser;
   runApp(
     MaterialApp(
-      home: loginWindow(),
+      home: (currentUsr == null) ? loginWindow() : mainWidget(),
     ),
   );
 }
