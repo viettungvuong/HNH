9243d45a05098ebc192808c3316c7d40f864c810
viettung <viettungvuong@gmail.com>
2022-06-12 00:31:30 +0700

    Chinh diary

--- lib/diary.dart
+++ lib/diary.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:hnh/main.dart';
 import 'package:intl/intl.dart';
+import 'package:sizer/sizer.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 import 'login.dart';
@@ -87,135 +88,146 @@ class _diaryState extends State<diaryWindow> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/images/Trangch.png'),
-                fit: BoxFit.cover),
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return MaterialApp(
+          home: Scaffold(
+            body: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/Trangch.png'),
+                    fit: BoxFit.cover),
+              ),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          alignment: Alignment.topLeft,
+                          padding:
+                              EdgeInsets.only(top: 5.h, left: 5.h, right: 50.w),
+                          child: Transform.scale(
+                            scale: 1.5,
+                            child: IconButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => mainWidget()),
+                                  );
+                                },
+                                icon: Image.asset('assets/images/Logo11.png')),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.topRight,
+                          padding: EdgeInsets.only(top: 5.h, right: 10.w),
+                          child: Transform.scale(
+                            scale: 1.5,
+                            child: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    showCalendar =
+                                        ((showCalendar) ? false : true);
+                                  });
+                                },
+                                icon:
+                                    Image.asset('assets/images/Calendar.png')),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      alignment: Alignment.topCenter,
+                      padding: EdgeInsets.only(top: 20),
+                      child: Text(
+                        date,
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Visibility(
+                      visible: (showCalendar),
+                      child: Container(
+                        alignment: Alignment.topCenter,
+                        child: TableCalendar(
+                          firstDay: DateTime.utc(2010, 10, 16),
+                          lastDay: DateTime.utc(2040, 3, 14),
+                          focusedDay: focused,
+                          selectedDayPredicate: (day) =>
+                              isSameDay(selected, day),
+                          onPageChanged: (focusedDay) {
+                            setState(() {
+                              focused = focusedDay;
+                            });
+                          },
+                          onDaySelected: (selectedDay, focusedDay) {
+                            setState(() {
+                              focused = focusedDay;
+                              selected = selectedDay;
+                              _controller.clear();
+                              openDate(focused); //mo coi diary cua ngay do
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.vertical,
+                        child: Text(
+                          tamSu,
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Row(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.only(left: 3.w, bottom: 5.h),
+                            width: 80.w,
+                            alignment: Alignment.bottomLeft,
+                            child: TextField(
+                              enableSuggestions: false,
+                              autocorrect: false,
+                              onSubmitted: (value) {
+                                write(_controller.text);
+                              },
+                              keyboardType: TextInputType.multiline,
+                              maxLines: 3,
+                              controller: _controller,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: 'Hãy tâm sự gì đó',
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(left: 5.w, bottom: 10.h),
+                            alignment: Alignment.bottomRight,
+                            child: Transform.scale(
+                              scale: 1.5,
+                              //bat buoc phai quang vao sizedbox vi iconbutton can parent
+                              child: IconButton(
+                                onPressed: () {
+                                  write(_controller.text);
+                                  _controller.clear();
+                                },
+                                icon: Image.asset('assets/images/send.png'),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ]),
+            ),
           ),
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Row(
-              children: [
-                Container(
-                  alignment: Alignment.topLeft,
-                  padding: EdgeInsets.only(top: 60, left: 30, right: 260),
-                  child: Transform.scale(
-                    scale: 1.5,
-                    child: IconButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => mainWidget()),
-                          );
-                        },
-                        icon: Image.asset('assets/images/Logo11.png')),
-                  ),
-                ),
-                Container(
-                  alignment: Alignment.topRight,
-                  padding: EdgeInsets.only(top: 60, right: 30),
-                  child: Transform.scale(
-                    scale: 1.5,
-                    child: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            showCalendar = ((showCalendar) ? false : true);
-                          });
-                        },
-                        icon: Image.asset('assets/images/Calendar.png')),
-                  ),
-                ),
-              ],
-            ),
-            Container(
-              alignment: Alignment.topCenter,
-              padding: EdgeInsets.only(top: 20),
-              child: Text(
-                date,
-                style: TextStyle(fontSize: 30),
-              ),
-            ),
-            Visibility(
-              visible: (showCalendar),
-              child: Container(
-                alignment: Alignment.topCenter,
-                child: TableCalendar(
-                  firstDay: DateTime.utc(2010, 10, 16),
-                  lastDay: DateTime.utc(2040, 3, 14),
-                  focusedDay: focused,
-                  selectedDayPredicate: (day) => isSameDay(selected, day),
-                  onPageChanged: (focusedDay) {
-                    setState(() {
-                      focused = focusedDay;
-                    });
-                  },
-                  onDaySelected: (selectedDay, focusedDay) {
-                    setState(() {
-                      focused = focusedDay;
-                      selected = selectedDay;
-                      _controller.clear();
-                      openDate(focused); //mo coi diary cua ngay do
-                    });
-                  },
-                ),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 40),
-              child: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Text(
-                  tamSu,
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-            ),
-            Expanded(
-              child: Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.only(left: 30, bottom: 40, right: 0),
-                    width: 350,
-                    alignment: Alignment.bottomLeft,
-                    child: TextField(
-                      enableSuggestions: false,
-                      autocorrect: false,
-                      onSubmitted: (value) {
-                        write(_controller.text);
-                      },
-                      keyboardType: TextInputType.multiline,
-                      maxLines: 3,
-                      controller: _controller,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Hãy tâm sự gì đó',
-                      ),
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(left: 20, bottom: 50),
-                    alignment: Alignment.bottomRight,
-                    child: Transform.scale(
-                      scale: 1.5,
-                      //bat buoc phai quang vao sizedbox vi iconbutton can parent
-                      child: IconButton(
-                        onPressed: () {
-                          write(_controller.text);
-                          _controller.clear();
-                        },
-                        icon: Image.asset('assets/images/send.png'),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ]),
-        ),
-      ),
+        );
+      },
     );
   }
 }
